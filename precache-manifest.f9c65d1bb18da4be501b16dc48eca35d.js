694b3d5ea98ad48e550b3189e416082947b70e63
IsaRodz <rodrigueziriarte96@gmail.com>
2020-04-15 14:54:06 -0500

    First Commit

---
+++ precache-manifest.f9c65d1bb18da4be501b16dc48eca35d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce0720078f8507418a2803dbbb6247d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f0c9d57b2be734a51eb",
+    "url": "/static/css/main.cc038111.chunk.css"
+  },
+  {
+    "revision": "534a9548f500275da345",
+    "url": "/static/js/2.532ec86a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.532ec86a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0c9d57b2be734a51eb",
+    "url": "/static/js/main.19cd2b52.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "c20a7ca2d826df431f2ff8d7c440c52f",
+    "url": "/static/media/bmwpremium.c20a7ca2.png"
+  },
+  {
+    "revision": "873c404952376e3fd0d1f62088f84da6",
+    "url": "/static/media/css3.873c4049.svg"
+  },
+  {
+    "revision": "a53714c0cf415f92a8a30483f047acf1",
+    "url": "/static/media/dekoclick.a53714c0.png"
+  },
+  {
+    "revision": "3b59ea6a4b1d3a2a385fc625f1664039",
+    "url": "/static/media/entresillas.3b59ea6a.png"
+  },
+  {
+    "revision": "19225d9b0014b5155a34e6b8e819ebbb",
+    "url": "/static/media/figma.19225d9b.svg"
+  },
+  {
+    "revision": "63f1b40cc07c9bc04f5319ce9e5be7f7",
+    "url": "/static/media/gadelgroup.63f1b40c.png"
+  },
+  {
+    "revision": "05afd8bdf7d97f7c46b6f54db8c18624",
+    "url": "/static/media/grupoicono.05afd8bd.png"
+  },
+  {
+    "revision": "940a1fd75579329ad513bfbe536eb181",
+    "url": "/static/media/html5.940a1fd7.svg"
+  },
+  {
+    "revision": "b990f91c660a47430d860c0f023506eb",
+    "url": "/static/media/illustrator.b990f91c.svg"
+  },
+  {
+    "revision": "7b1bdce1f5f60715d8e13b2e2729c2e3",
+    "url": "/static/media/js.7b1bdce1.svg"
+  },
+  {
+    "revision": "7fd6f7525a3f14558719a7a42e62f1da",
+    "url": "/static/media/lines1.7fd6f752.png"
+  },
+  {
+    "revision": "927e651ae13c023c784b9a7fc73c4c68",
+    "url": "/static/media/mail.927e651a.svg"
+  },
+  {
+    "revision": "326477621245a6e2d38e2f38868db8e4",
+    "url": "/static/media/phone.32647762.svg"
+  },
+  {
+    "revision": "671db62ea0955adb691c80223b9098b6",
+    "url": "/static/media/php.671db62e.svg"
+  },
+  {
+    "revision": "b60f8447ce255d0d21253316fda66274",
+    "url": "/static/media/premiumsalud.b60f8447.png"
+  },
+  {
+    "revision": "f1f1f732c533fbb5bc13f4057373481d",
+    "url": "/static/media/react.f1f1f732.svg"
+  },
+  {
+    "revision": "805780b0800d260e1688f8a0b3d8b48a",
+    "url": "/static/media/sass.805780b0.svg"
+  },
+  {
+    "revision": "98a2ff701eceb6876b19e4ca82b93ab2",
+    "url": "/static/media/vuejs.98a2ff70.svg"
+  },
+  {
+    "revision": "6b6a71adfb7f1477a9bb6496d440b126",
+    "url": "/static/media/wordpress.6b6a71ad.svg"
+  }
+]);
